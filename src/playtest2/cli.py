a2bad6f1dcf54c7d811538d2bac3676affcba68d
ftnext <takuyafjp+develop@gmail.com>
2025-12-07 22:49:41 +0900

    [feat] Users can specify step_impl directories

--- src/playtest2/cli.py
+++ src/playtest2/cli.py
@@ -3,9 +3,11 @@ import sys
 from pathlib import Path
 
 
-def setup(_):
+def setup(args):
     python_properties_dir = Path.cwd() / "env" / "default"
     python_properties_dir.mkdir(parents=True, exist_ok=True)
+
+    step_impl_dirs: list[str] = []
     playtest2_steps_dir = (
         Path(sys.prefix)
         / "lib"
@@ -13,9 +15,13 @@ def setup(_):
         / "site-packages"
         / "playtest2"
     )
+    step_impl_dirs.append(str(playtest2_steps_dir))
+
+    step_impl_dirs.extend(str(path) for path in args.step_impl_dir)
+
     (python_properties_dir / "python.properties").write_text(
         f"""GAUGE_PYTHON_COMMAND = python
-STEP_IMPL_DIR = {playtest2_steps_dir}"""
+STEP_IMPL_DIR = {",".join(step_impl_dirs)}"""
     )
 
 
@@ -24,6 +30,7 @@ def main():
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     setup_parser = subparsers.add_parser("setup", help="Set up gauge environment")
+    setup_parser.add_argument("step_impl_dir", type=Path, nargs="*", help="Step implementation directory")
     setup_parser.set_defaults(func=setup)
 
     args = parser.parse_args()
