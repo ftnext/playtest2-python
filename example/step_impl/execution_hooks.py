3bf6d1eb3088f4032f050e6fccf90a8455603620
ftnext <takuyafjp+develop@gmail.com>
2025-12-08 00:05:45 +0900

    [refactor] Write short

--- example/step_impl/execution_hooks.py
+++ example/step_impl/execution_hooks.py
@@ -13,12 +13,8 @@ def hello():
     return {"message": "Hello World"}
 
 
-config = uvicorn.Config(app, port=8000)
-server = uvicorn.Server(config)
-
-
 @before_suite
 def start_app():
-    server_thread = threading.Thread(target=server.run, daemon=True)
+    server_thread = threading.Thread(target=uvicorn.run, args=(app,), kwargs={"port": 8000}, daemon=True)
     server_thread.start()
     time.sleep(1)
